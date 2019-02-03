e16b3a90be9bee98c3b7402f70ccecbcaf344e1b
Patrick McKernin <pmckernin@gmail.com>
2019-02-03 18:34:42 +0000

    all weather done

--- app/controllers/geocoding_controller.rb
+++ app/controllers/geocoding_controller.rb
@@ -5,7 +5,7 @@ class GeocodingController < ApplicationController
     @street_address = params.fetch("user_street_address")
     sanitized_street_address = URI.encode(@street_address)
 
-    url = "https://maps.googleapis.com/maps/api/geocode/json?address=5807+S+Woodlawn+Ave&key=AIzaSyBr-0XDfztIIUGyPRfa1D5KfPvURvAk2e4"
+    url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + @street_address.to_s + "&key=AIzaSyBr-0XDfztIIUGyPRfa1D5KfPvURvAk2e4"
     parsed_data = JSON.parse(open(url).read)
     lat = parsed_data.dig("results", 0, "geometry", "location", "lat")
     long = parsed_data.dig("results", 0, "geometry", "location", "lng")
--- app/controllers/meteorologist_controller.rb
+++ app/controllers/meteorologist_controller.rb
@@ -5,22 +5,25 @@ class MeteorologistController < ApplicationController
     @street_address = params.fetch("user_street_address")
     sanitized_street_address = URI.encode(@street_address)
 
-    # ==========================================================================
-    # Your code goes below.
-    # The street address the user input is in the string @street_address.
-    # A sanitized version of the street address, with spaces and other illegal
-    #   characters removed, is in the string sanitized_street_address.
-    # ==========================================================================
+     url1 = "https://maps.googleapis.com/maps/api/geocode/json?address=" + @street_address.to_s + "&key=AIzaSyBr-0XDfztIIUGyPRfa1D5KfPvURvAk2e4"
+    parsed_data_street = JSON.parse(open(url1).read)
+    lat = parsed_data_street.dig("results", 0, "geometry", "location", "lat")
+    long = parsed_data_street.dig("results", 0, "geometry", "location", "lng")
+    
+       url = "https://api.darksky.net/forecast/8237bf7229ea18173816ae7ffe2da7cd/" + lat.to_s + "," + long.to_s
+       parsed_data_weather = JSON.parse(open(url).read)
 
-    @current_temperature = "Replace this string with your answer"
 
-    @current_summary = "Replace this string with your answer"
+   
+    @current_temperature = parsed_data_weather.dig("currently", "temperature")
 
-    @summary_of_next_sixty_minutes = "Replace this string with your answer"
+    @current_summary = parsed_data_weather.dig("currently", "summary")
 
-    @summary_of_next_several_hours = "Replace this string with your answer"
+    @summary_of_next_sixty_minutes = parsed_data_weather.dig("minutely", "summary")
 
-    @summary_of_next_several_days = "Replace this string with your answer"
+    @summary_of_next_several_hours = parsed_data_weather.dig("hourly", "summary")
+
+    @summary_of_next_several_days = parsed_data_weather.dig("daily", "summary")
 
     render("meteorologist_templates/street_to_weather.html.erb")
   end
